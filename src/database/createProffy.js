9991f85194b589fc50b69e2582d1649c1ac6b1fd
Gregory Ferreira <gregoryflucena@gmail.com>
2020-09-07 16:00:20 -0300

    banco de dados criado

--- src/database/createProffy.js
+++ src/database/createProffy.js
@@ -1,1 +1,53 @@
-createProffy.js  db.js	test.js
+module.exports = async function(db, { proffyValue, classValue, classScheduleValues }) {
+  // Inserir dados na tabela de teachers
+  const insertProffy = await db.run(`
+    INSERT INTO proffys (
+      name,
+      avatar,
+      whatsapp,
+      bio
+    ) VALUES (
+      "${proffyValue.name}",
+      "${proffyValue.avatar}",
+      "${proffyValue.whatsapp}",
+      "${proffyValue.bio}"
+    )
+  `)
+
+  const proffy_id = insertProffy.lastID
+
+  //  Inserir dados na tabela classes
+  const insertedClass = await db.run(`
+      INSERT INTO classes (
+        subject,
+        cost,
+        proffy_id
+      ) VALUES (
+        "${classValue.subject}",
+        "${classValue.cost}",
+        "${proffy_id}"
+      );
+  `)
+
+  const class_id = insertedClass.lastID //insertedProffy.lastID
+
+  // Inserir dados na tabela class_schedule
+  const insertedAllClassesScheduleValues = classScheduleValues.map((classScheduleValue) => {
+      return db.run(`
+        INSERT INTO class_schedule (
+          class_id,
+          weekday,
+          time_from,
+          time_to
+        ) VALUES (
+          "${class_id}",
+          "${classScheduleValue.weekday}",
+          "${classScheduleValue.time_from}",
+          "${classScheduleValue.time_to}"
+        );
+      `)
+  })
+
+  // Aqui vou executar todos os db runs() das class_schedules
+  await Promise.all(insertedAllClassesScheduleValues)
+}
