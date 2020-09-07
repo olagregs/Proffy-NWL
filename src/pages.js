6172ed7400c04471d26ad1c0f59af6348737ab0e
Gregory Ferreira <gregoryflucena@gmail.com>
2020-09-07 10:09:17 -0300

    configuração das páginas e do servidor adicionadas

--- src/pages.js
+++ src/pages.js
@@ -1,1 +1,96 @@
-database  pages.js  server.js  utils  views
+const Database = require('./database/db')
+
+const { subjects, weekdays, getSubjects, convertHoursToMinutes, } = require('./utils/format')
+
+function landingPage(req, res) {
+  return res.render("index.html")
+}
+
+async function studyPage(req, res) {
+  const filters = req.query
+
+  if (!filters.subject || !filters.weekday || !filters.time) {
+    return res.render("study-page.html", { filters, subjects, weekdays })
+  }
+
+  // Converter horas em minutos
+  const timeToMinutes = convertHoursToMinutes(filters.time)
+
+  const query = `
+    SELECT classes.*, proffys.*
+    FROM proffys
+    JOIN classes ON (classes.proffy_id = proffys.id)
+    WHERE EXISTS (
+      SELECT class_schedule.*
+      FROM class_schedule
+      WHERE class_schedule.class_id = classes.id
+      AND class_schedule.weekday = ${filters.weekday}
+      AND class_schedule.time_from <= ${timeToMinutes}
+      AND class_schedule.time_to > ${timeToMinutes}
+    )
+    AND classes.subject = '${filters.subject}'
+  `
+  // Caso haja erros na hora da consulta do banco de dados
+  try {
+    const db = await Database
+    const proffys = await db.all(query)
+
+      proffys.map((proffy) => {
+        proffy.subject = getSubjects(proffy.subject)
+      })
+
+    return res.render('study-page.html', { proffys, subjects, filters, weekdays })
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+function giveClassesPage(req, res) {
+  // Se não, mostrar a mesma página
+  return res.render("give-classes.html", { subjects, weekdays })
+}
+
+async function saveClasses(req, res) {
+  const createProffy = require('./database/createProffy')
+
+  const proffyValue = {
+    name: req.body.name,
+    avatar: req.body.avatar,
+    whatsapp: req.body.whatsapp,
+    bio: req.body.bio
+  }
+
+  const classValue = {
+    subject: req.body.subject,
+    cost: req.body.cost
+  }
+
+  const classScheduleValues = req.body.weekday.map((weekday, index) => {
+    return {
+      weekday,
+      time_from: convertHoursToMinutes(req.body.time_from[index]),
+      time_to: convertHoursToMinutes(req.body.time_to[index])
+    }
+  })
+
+  try {
+     const db = await Database
+     await createProffy(db, { proffyValue, classValue, classScheduleValues })
+
+      let queryString = "?subject=" + req.body.subject
+      queryString += "&weekday=" + req.body.weekday[0]
+      queryString += "&time=" + req.body.time_from[0]
+
+     return res.redirect("/study-page" + queryString)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = {
+  landingPage,
+  studyPage,
+  giveClassesPage,
+  saveClasses
+}
--- src/server.js
+++ src/server.js
@@ -1,1 +1,32 @@
-database  server.js  utils  views
+// Servidor
+const express = require("express")
+const server = express()
+
+const { 
+  landingPage,
+  studyPage,
+  giveClassesPage,
+  saveClasses
+} = require('./pages')
+
+// Configura o nunjucks (template engine)
+const nunjucks = require("nunjucks")
+nunjucks.configure('src/views', {
+  express: server,
+  noCache: true,
+})
+
+// Inicia o servidor
+server
+
+// Receber os dados do req.body
+.use(express.urlencoded({extended: true}))
+// Configura arquivos estáticos  (css, script, imagens)
+.use(express.static("public"))
+// Rotas da aplicação
+.get("/", landingPage)
+.get("/study-page", studyPage)
+.get("/give-classes", giveClassesPage)
+.post("/save-classes", saveClasses)
+// Start do servidor
+.listen(5500)
